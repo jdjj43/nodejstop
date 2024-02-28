ab6dc2dc6a6335d2e7e5795d893562ccfe3eee83
jdjj43 <jjaedej@gmail.com>
2024-02-27 20:40:58 -0400

    1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+});
+
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(__dirname + '/contact-me.html');
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/404.html');
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log(
+        'Server Up!'
+    );
+});
